cc09b30034c1ec328e95ff7cc3c554c89498f62d
fennystt <60440417+fennystt@users.noreply.github.com>
2020-02-09 02:40:48 +0700

    Add files via upload

---
+++ gojek.php
@@ -0,0 +1,53 @@
+<?php
+
+error_reporting(0);
+if (!file_exists('token')) {
+    mkdir('token', 0777, true);
+}
+
+include ("curl.php");
+echo "\n";
+echo "\e[94m            NOT SAFE FOR WORK IF2               \n";
+echo "\e[91m FORMAT NOMOR HP : INDONESIA '62***' , US='1***'\n";
+echo "\e[93m SCRIPT CAU ITEM\n";
+echo "\n";
+echo "\e[96m[?] Masukkan Nomor HP Anda (62/1) : ";
+$nope = trim(fgets(STDIN));
+$register = register($nope);
+if ($register == false)
+    {
+    echo "\e[91m[x] Nomor Telah Terdaftar\n";
+    }
+  else
+    {
+    otp:
+    echo "\e[96m[!] Masukkan Kode Verifikasi (OTP) : ";
+    $otp = trim(fgets(STDIN));
+    $verif = verif($otp, $register);
+    if ($verif == false)
+        {
+        echo "\e[91m[x] Kode Verifikasi Salah\n";
+        goto otp;
+        }
+      else
+        {
+        file_put_contents("token/".$verif['data']['customer']['name'].".txt", $verif['data']['access_token']);
+        echo "\e[93m[!] Trying to redeem Voucher : GOPAYTANGO !\n";
+        sleep(3);
+        $claim = claim($verif);
+        goto pengen;
+            }
+            pengen:
+            $claim = cekvocer($verif);
+            if ($claim == false ) {
+                echo "\033VOUCHER INVALID/GAGAL REDEEM\n";
+            }
+            else{
+                echo "\e[92m[+] ".$claim."\n";
+                
+        }
+    }
+    }
+
+
+?>
